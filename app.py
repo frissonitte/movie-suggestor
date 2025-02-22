1d34f7daec5de7ee09eca6b958f0b2f355680e62
frissonitte <emirhan0yildirim@gmail.com>
2025-02-22 19:49:33 +0300

    Database and app.py created.

---
+++ app.py
@@ -0,0 +1,25 @@
+import sqlite3
+from flask import Flask, g, render_template, request, redirect
+
+app = Flask(__name__)
+
+DATABASE = "movie.db"
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ setup_database.py
@@ -0,0 +1,63 @@
+import sqlite3
+import pandas as pd
+
+#Loading dataset
+movies = pd.read_csv('data/movies.csv')
+ratings = pd.read_csv('data/ratings.csv')
+links = pd.read_csv('data/links.csv')
+tags = pd.read_csv('data/tags.csv')
+
+#Creating SQL database
+conn = sqlite3.connect('movie.db')
+cursor = conn.cursor()
+
+#Creating database tables
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS movies (
+        movieId INTEGER PRIMARY KEY,
+        movie_title TEXT NOT NULL,
+        genres TEXT NOT NULL
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS ratings (
+        userId INTEGER,
+        movieId INTEGER,
+        rating REAL,
+        timestamp INTEGER,
+        FOREIGN KEY (movieId) REFERENCES movies (movieId)
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS links (
+        movieId INTEGER,
+        imdbId INTEGER,
+        tmdbId INTEGER,
+        FOREIGN KEY (movieId) REFERENCES movies (movieId)
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tags (
+        movieId INTEGER,
+        userId INTEGER,
+        tag TEXT NOT NULL,
+        timestamp INTEGER,
+        FOREIGN KEY (movieId) REFERENCES movies (movieId)
+        FOREIGN KEY (userId) REFERENCES movies (userId)
+    )
+''')
+
+
+#Data insertation
+movies.to_sql('movies',conn, if_exists='replace', index=False)
+ratings.to_sql('ratings',conn, if_exists='replace', index=False)
+links.to_sql('links', conn, if_exists='replace', index=False)
+tags.to_sql('tags', conn, if_exists='replace', index=False)
+
+conn.commit()
+conn.close()
+
+print("Database created and populated successfully!")
