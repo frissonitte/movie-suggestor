fbd6c0327de375348c0d5379ff4847fe2f781178
frissonitte <emirhan0yildirim@gmail.com>
2025-02-22 21:17:38 +0300

    Setted up app.py and a few templates.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g, render_template, request, redirect
+from flask import Flask, g, render_template, request, redirect, abort
 
 app = Flask(__name__)
 
@@ -9,11 +9,24 @@ def get_db():
     db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
     return db
 
+
 @app.route("/")
-def home():
-    return "Hello, World!"
+def index():
+   return render_template("index.html")
+
+@app.route("/movies", methods=["GET", "POST"])
+def movies():
+    movies = get_db().execute(
+        "SELECT title, genres FROM movies WHERE genres LIKE '%Comedy%'"
+    ).fetchall()
+    
+    if not movies:
+        abort(404, description="No movies found in this genre.")
+    
+    return render_template("movies.html",movies=movies)
 
 @app.teardown_appcontext
 def close_connection(exception):
